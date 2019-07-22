ae5de77ae1597a232633f9119a29364dbc1c05f7
William McCausland <mccauslw@mac.com>
2019-07-22 12:11:08 -0400

    another minor change

--- R/T_1969_data.R
+++ R/T_1969_data.R
@@ -1,8 +1,8 @@
 #' Choice count data from the Tversky (1969) experiment
 #'
 #' An 8 by 31 by 5 array of count data.
-#' Element \eqn{i,j,k} gives the number of times subject \eqn{i} chose object
-#' \eqn{k} when presented with choice set \eqn{j}.
+#' Element \code{T_1969_counts[i,j,k]} gives the number of times subject \code{i} chose object
+#' \code{k} when presented with choice set \code{j}.
 #' The choice set index \eqn{j \in \{1,...,7\}} encodes a non-empty subset of the
 #' universe of the three choice objects \eqn{x}, \eqn{y}, and \eqn{z}, numbered 1, 2, 3.
 #' Whenever \eqn{k} is not an element of \eqn{j}, the value is \code{NA}.
