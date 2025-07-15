e8db8d9fa7a840b116e20f7f906f38bec4beded3
William McCausland <mccauslw@mac.com>
2025-07-15 12:00:40 -0400

    Corrections and improvements to likelihoods.R Much more extensive testing in test_likelihoods.R

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -1,4 +1,151 @@
-## Density and random generation for distributions of the RC model P and
+## Basic construction of Dirichlet shape parameters for all menus of a universe
+
+#' Dirichlet shape parameters for all menus of a universe, with constant values
+#'
+#' \code{DirRC_constant_shape} returns a matrix of Dirichlet shape
+#' parameters where all individual shape parameters have the same value
+#' \code{alpha}, within and across menus.
+#' @param n_objects number of objects in the universe
+#' @param alpha the common value of the shape parameters
+#' @param name_source universe or matrix to extract object and menu notes from
+#' @return a matrix of Dirichlet shape parameters with all values set to
+#' \code{alpha}. If \code{alpha} is one, the Dirichlet distribution on each
+#' choice probability vector coincides with the uniform distribution.
+#' @examples
+#' Alpha <- DirRC_constant_shape(3, 2.0)
+#' P <- rDirichletRC(10, Alpha)
+#' @export
+DirRC_constant_shape <- function(n_objects, alpha, name_source = NULL) {
+  n_subsets <- 2^n_objects-1
+  Alpha <- alpha * u_const$member_table[1:n_subsets, 1:n_objects]
+  dimnames(Alpha) <- copy_A_x_names(name_source, n_subsets, n_objects)
+  Alpha
+}
+
+#' Dirichlet shape parameters for all menus of a universe, with constant menu sums
+#'
+#' \code{DirRC_constant_sum} returns a matrix of Dirichlet shape parameters
+#' for a vector Dirichlet prior for a random choice structure.
+#' @param weights_spec either a vector of object weights or the number of
+#' equal weights. Shape parameters are proportional to these weights, menu by
+#' menu.
+#' @param alpha_sum scalar, sum of shape parameters, constant over all menus
+#' @param name_source universe or matrix to extract object and menu names from
+#' @return a matrix of Dirichlet shape parameters where each row A gives
+#' the parameter vector for menu A.
+#' @examples
+#' Alpha <- DirRC_constant_sum(c(2.5, 1.2, 3.3), 5.2)
+#' P <- rDirichletRC(10, Alpha)
+#' @export
+DirRC_constant_sum <- function(weights_spec, alpha_sum, name_source = NULL) {
+
+  stopifnot(is.numeric(weights_spec))
+
+  if (length(weights_spec)==1) weights_spec <- rep(1.0, weights_spec)
+
+  n_objects <- length(weights_spec)
+  n_subsets <- 2^n_objects-1
+  Alpha = matrix(0, nrow=n_subsets, ncol=n_objects)
+  for (A in 1:n_subsets) {
+    den <- sum(weights_spec[u_const$subset_vectors[[A]]])
+    Alpha[A, u_const$subset_vectors[[A]]] =
+      alpha_sum * weights_spec[u_const$subset_vectors[[A]]]/den
+  }
+  Alpha <- Alpha * u_const$member_table[1:n_subsets, 1:n_objects]
+  dimnames(Alpha) <- copy_A_x_names(name_source, n_subsets, n_objects)
+  Alpha
+}
+
+## Basic RCS construction
+
+#' Random Choice Structure with all probabilities discrete uniform
+#'
+#' \code{P_uniform} constructs a Random Choice Structure (RCS) for a given
+#' universe size, where all choice probabilities are discrete uniform.
+#' @param n_objects integer, number of objects in universe
+#' @return RCS where all choice probabilities are discrete uniform
+#' @examples
+#' P <- P_uniform(3)
+#' @export
+P_uniform <- function(n_objects) {
+  n_subsets <- 2^n_objects - 1
+  P <- u_const$member_table[1:n_subsets, 1:n_objects]
+  for (i in 1:n_subsets) {
+    P[i, ] <- P[i, ]/u_const$subset_card[i]
+  }
+  P
+}
+
+#' Construct a Luce model
+#'
+#' \code{P_Luce} constructs a random choice structure (RCS) from a Luce
+#' model.
+#' @param v vector specifying non-negative Luce weights for a collection of
+#' objects.
+#' If \code{v} is named, the names are interpreted as the names of the objects,
+#' and row and column names of the output will be set accordingly.
+#' @returns A Random Choice Structure with choice probabilities set in proportion
+#' to Luce weights.
+#' @export
+#' @examples
+#' P <- P_Luce(c(1,2,3))
+#' P <- P_Luce(c(a=1, b=2, c=3)) # Same, but sets object names
+#' @seealso [P_logit()]
+P_Luce <- function(v) {
+  stopifnot(all(v >= 0))
+  n_objects <- length(v)
+  n_subsets <- 2^n_objects - 1
+  P <- u_const$member_table[1:n_subsets, 1:n_objects]
+  for (i in 1:n_subsets) {
+    P[i, ] <- P[i, ] * v / sum(v * u_const$member_table[i, ], na.rm=TRUE)
+  }
+  if (!is.null(names(v))) {
+    rownames(P) <- menu_names(names(v))
+    colnames(P) <- names(v)
+  }
+  P
+}
+
+#' Construct a logit model
+#'
+#' \code{P_logit} constructs a random choice structure (RCS) for a logit model.
+#' @param x vector of logit values for a collection of objects.
+#' If \code{x} is named, the names are interpreted as the names of the objects,
+#' and the row and column names of the output will be set accordingly.
+#'
+#' @returns A Random Choice Structure with choice probabilities set in
+#' proportion to the exponential of the logit values.
+#' @export
+#'
+#' @examples
+#' P <- P_logit(c(-2, 0, 1))
+#' P <- P_logit(c(a=-2, b=0, c=1)) # Same, but sets object names
+#' @seealso [P_Luce()]
+P_logit <- function(x) {
+  P <- P_Luce(exp(x))
+}
+
+#' Random Choice Structure from count proportions
+#'
+#' \code{P_frequencies} takes a count matrix as input, and returns choice
+#' frequencies as a random choice structure.
+#'
+#' @param N A count matrix.
+#' @return A random choice structure with choice probabilities equal to
+#' empirical choice frequencies
+#' @export
+#'
+#' @examples
+#' P <- P_frequencies(MMS_2019_counts[1,,])
+P_frequencies <- function(N) {
+  n_objects <- ncol(N)
+  P <- N/rowSums(N, na.rm = TRUE)
+  for (i in 1:n_objects)
+    P[u_const$singletons[i], i] <- 1.0
+  P
+}
+
+# Density and random generation for distributions of the RC model P and
 ## for distributions of RC count data N
 
 #' Compute multinomial coefficient
@@ -30,13 +177,15 @@ multinomial_coef <- function(counts, log = TRUE) {
 #' @return a random count matrix (if \code{n_draws} = 1) or array (if \code{n_draws} > 1).
 #' The first two dimensions are indexed by choice set A and object x.
 #' @examples
-#' u <- c(1, 2, 3); n_objects=3; n_subsets=2^n_objects-1;
-#' P <- P_Luce(u)
-#' xy <- set_index(c(1, 2))      # Choice set with x and y
-#' xyz <- set_index(c(1, 2, 3))  # Choice set with x, y, and z
+#' v <- c(a=1, b=2, c=3)
+#' n_objects <- length(v)
+#' n_subsets <- 2^n_objects-1;
+#' P <- P_Luce(v)
+#' xy <- set_index(c(1, 2))      # Menu with x and y
+#' xyz <- set_index(c(1, 2, 3))  # Menu with x, y, and z
 #' N_total <- vector(mode="integer", length=n_subsets)
-#' N_total[xy] <- 10
-#' N_total[xyz] <- 10
+#' N_total[xy] <- 10    # Number of trials for menu with x and y
+#' N_total[xyz] <- 10   # Number of trials for menu with x, y, and z
 #' N <- rmultinomRC(5, P, N_total)
 #' print(N[, , 1], na.print='-') # Print first count matrix
 #' dmultinomRC(P, N[, , 1], categorical=TRUE, log=TRUE)
@@ -87,14 +236,13 @@ rmultinomRC <- function(n_draws, P, N_total) {
 #' @export
 dmultinomRC <- function(P, N, categorical=FALSE, log=TRUE) {
   stopifnot(identical(dim(P), dim(N)[1:2]))
-  if (is.matrix(N)) dim(N) <- c(dim(N), 1)
-  n_objects <- ncol(P)
+  if (is.matrix(N)) dim(N) <- c(dim(N), 1) # Coerce to 3-D array
   ln_L <- 0
   for (i in seq_len(dim(N)[3])) {
     for (A in 1:nrow(P)) {
       if (u_const$subset_card[A] > 1) {
         v <- u_const$subset_vectors[[A]]
-        v <- v[N[A,v,i] != 0]
+        v <- v[N[A, v, i] != 0]
         if (length(v) > 0) {
           if (categorical)
             ln_L <- ln_L + sum(N[A, v, i] * log(P[A, v]))
@@ -117,8 +265,8 @@ dmultinomRC <- function(P, N, categorical=FALSE, log=TRUE) {
 #' If the returned matrix is \code{P}, \code{P[, , i]} is the \eqn{i}'th draw
 #' of a RCS.
 #' @examples
-#' Alpha = DirRC_constant_shape(3, 1.0)
-#' P <- rDirichletRC(10, Alpha) # 10 draws, 3 objects in universe
+#' Alpha <- DirRC_constant_shape(3, 1.0) # 3 objects in universe
+#' P <- rDirichletRC(10, Alpha) # 10 draws
 #' @importFrom extraDistr rdirichlet
 #' @export
 rDirichletRC <- function(n_draws, Alpha) {
@@ -152,8 +300,6 @@ dDirichletRC <- function(Alpha, P, log=TRUE) {
   for (A in 1:nrow(Alpha)) {
     x <- u_const$subset_vectors[[A]]
     if (u_const$subset_card[A] > 1) {
-      print(P[A, x, ])
-      print(Alpha[A, x])
       ln_L <- ln_L + extraDistr::ddirichlet(t(P[A, x, ]), Alpha[A, x], log=TRUE)
     }
   }
@@ -188,15 +334,13 @@ dDirichletRC <- function(Alpha, P, log=TRUE) {
 #' @export
 rDirMultinomRC <- function(n_draws, Alpha, N_total) {
   stopifnot(nrow(Alpha)==length(N_total))
-  N <- array(dim=c(n_draws, dim(Alpha)), dimnames=c(list(NULL), dimnames(Alpha)))
-  for (i in 1:nrow(Alpha)) {
-    v <- u_const$subset_vectors[[i]]
-    if (u_const$subset_card[i] > 1) {
-      for (draw in 1:n_draws) { # Need fresh draw of p, saving memory at cost of speed
-        N[draw, i, v] <- extraDistr::rdirmnom(1, Alpha[i, v], N_total[i])
-      }
+  N <- array(dim=c(dim(Alpha), n_draws), dimnames=c(dimnames(Alpha), list(NULL)))
+  for (A in seq_len(nrow(Alpha))) {
+    v <- u_const$subset_vectors[[A]]
+    if (u_const$subset_card[A] > 1) {
+      N[A, v, ] <- t(extraDistr::rdirmnom(n_draws, N_total[A], Alpha[A, v]))
     } else {
-      N[, i, v] <- N_total[i]
+      N[A, v, ] <- N_total[A]
     }
   }
   N
@@ -218,159 +362,26 @@ rDirMultinomRC <- function(n_draws, Alpha, N_total) {
 #' @return Likelihood or log likelihood value
 #' @examples
 #' Alpha <- DirRC_constant_sum(3, 2.0)
-#' N <- T_1972_counts['Dots', 6,,]
+#' N <- T_1972_counts['Dots', 6, , ]
 #' dDirMultinomRC(Alpha, N)
 #' @seealso \code{\link{rDirMultinomRC}}, which simulates a count matrix under
 #' this model, given the total number of trials for each choice subset.
 #' @importFrom extraDistr ddirmnom
 #' @export
 dDirMultinomRC <- function(Alpha, N, categorical=FALSE, log=TRUE) {
-  stopifnot(identical(dim(Alpha), dim(N)))
+  stopifnot(identical(dim(Alpha), dim(N)[1:2]))
+  if (is.matrix(N)) dim(N) <- c(dim(N), 1) # Coerce to 3-D array
   ln_L <- 0
-  for (i in 1:nrow(Alpha)) {
-    if (u_const$subset_card[i] > 1) {
-      v <- u_const$subset_vectors[[i]]
-      ln_L <- ln_L + ddirmnom(N[i, v], sum(N[i, v]), Alpha[i, v], log=TRUE)
-      if (categorical) {
-        ln_L <- ln_L - multinomial_coef(N[i, v], log=TRUE)
+  for (i in seq_len(dim(N)[3])) {
+    for (A in 1:nrow(Alpha)) {
+      if (u_const$subset_card[A] > 1) {
+        v <- u_const$subset_vectors[[A]]
+        ln_L <- ln_L + ddirmnom(N[A, v, i], sum(N[A, v, i]), Alpha[A, v], log=TRUE)
+        if (categorical) {
+          ln_L <- ln_L - multinomial_coef(N[A, v, i], log=TRUE)
+        }
       }
     }
   }
   if (log) ln_L else exp(ln_L)
-}
-
-## Basic RCS construction
-
-#' P_uniform
-#'
-#' \code{P_uniform} constructs a random choice structure (RCS) where all
-#' choice probabilities are discrete uniform
-#' @param n_objects integer, number of objects in universe
-#' @return RCS where all choice probabilities are discrete uniform
-#' @examples
-#' P <- P_uniform(3)
-#' @export
-P_uniform <- function(n_objects) {
-  n_subsets <- 2^n_objects - 1
-  P <- u_const$member_table[1:n_subsets, 1:n_objects]
-  for (i in 1:n_subsets) {
-    P[i, ] <- P[i, ]/u_const$subset_card[i]
-  }
-  P
-}
-
-#' Construct a Luce model
-#'
-#' \code{P_Luce} constructs a random choice structure (RCS) from a Luce
-#' model.
-#' @param v positive vector, Luce weights for a vector of objects.
-#' If \code{v} is a named vector, the row and column names of the output will
-#' be set accordingly.
-#' @returns RCS with choice probabilities given by Luce weights
-#' @export
-#' @examples
-#' P <- P_Luce(c(1.2,2.5,3.3))
-P_Luce <- function(v) {
-  n_objects <- length(v)
-  n_subsets <- 2^n_objects - 1
-  P <- u_const$member_table[1:n_subsets, 1:n_objects]
-  for (i in 1:n_subsets) {
-    P[i, ] <- P[i, ] * v / sum(v * u_const$member_table[i, ], na.rm=TRUE)
-  }
-  if (!is.null(names(v))) {
-    rownames(P) <- menu_names(names(v))
-    colnames(P) <- names(v)
-  }
-  P
-}
-
-#' Construct a logit model
-#'
-#' \code{P_logit} constructs a random choice structure (RCS) for a logit model.
-#' @param x vector of logit values for a vector of objects.
-#' If \code{x} is a named vector, the row and column names of the output will
-#' be set accordingly.
-#'
-#' @returns A random choice structure.
-#' @export
-#'
-#' @examples
-#' P <- P_logit(c(a=-2, b=0, c=1))
-P_logit <- function(x) {
-  P <- P_Luce(exp(x))
-}
-
-#' Random Choice Structure from count proportions
-#'
-#' \code{P_frequencies} takes a count matrix as input, and returns choice
-#' frequencies as a random choice structure.
-#'
-#' @param N A count matrix.
-#' @return A random choice structure.
-#' @export
-#'
-#' @examples
-#' P <- P_frequencies(MMS_2019_counts[1,,])
-P_frequencies <- function(N) {
-  n_objects <- ncol(N)
-  P <- N/rowSums(N, na.rm = TRUE)
-  for (i in 1:n_objects)
-    P[u_const$singletons[i], i] <- 1.0
-  P
-}
-
-## Basic construction of Dirichlet shape parameters for all menus of a universe
-
-#' Dirichlet shape parameters for all menus of a universe, with constant values
-#'
-#' \code{DirRC_constant_shape} returns a matrix of Dirichlet shape
-#' parameters where all individual shape parameters have the same value
-#' \code{alpha}, within and across menus.
-#' @param n_objects number of objects in the universe
-#' @param alpha the common value of the shape parameters
-#' @param name_source universe or matrix (WJM: expand on this)
-#' @return a matrix of Dirichlet shape parameters with all values set to
-#' \code{alpha}. If \code{alpha} is one, the Dirichlet distribution on each
-#' choice probability vector coincides with the uniform distribution.
-#' @examples
-#' Alpha <- DirRC_constant_shape(3, 2.0)
-#' P <- rDirichletRC(10, Alpha)
-#' @export
-DirRC_constant_shape <- function(n_objects, alpha, name_source = NULL) {
-  n_subsets <- 2^n_objects-1
-  Alpha <- alpha * u_const$member_table[1:n_subsets, 1:n_objects]
-  dimnames(Alpha) <- copy_A_x_names(name_source, n_subsets, n_objects)
-  Alpha
-}
-
-#' Dirichlet shape parameters for all menus of a universe, with constant menu sums
-#'
-#' \code{DirRC_constant_sum} returns a matrix of Dirichlet shape parameters
-#' for a vector Dirichlet prior for a random choice structure.
-#' @param weights_spec either a vector of object weights or the number of
-#' equal weights. Gives the
-#' @param alpha_sum scalar, sum of shape parameters, constant over all menus
-#' @param name_source universe or matrix (WJM: expand on this)
-#' @return a matrix of Dirichlet shape parameters where each row A gives
-#' the parameter vector for menu A.
-#' @examples
-#' Alpha <- DirRC_constant_sum(c(2.5, 1.2, 3.3), 5.2)
-#' P <- rDirichletRC(10, Alpha)
-#' @export
-DirRC_constant_sum <- function(weights_spec, alpha_sum, name_source = NULL) {
-
-  stopifnot(is.numeric(weights_spec))
-  if (length(weights_spec)==1) weights_spec <- rep(1.0, weights_spec)
-
-  n_objects <- length(weights_spec)
-  n_subsets <- 2^n_objects-1
-  Alpha = matrix(0, nrow=n_subsets, ncol=n_objects)
-  for (A in 1:n_subsets) {
-    den <- sum(weights_spec[u_const$subset_vectors[[A]]])
-    Alpha[A, u_const$subset_vectors[[A]]] =
-      alpha_sum * weights_spec[u_const$subset_vectors[[A]]]/den
-  }
-  Alpha <- Alpha * u_const$member_table[1:n_subsets, 1:n_objects]
-  dimnames(Alpha) <- copy_A_x_names(name_source, n_subsets, n_objects)
-  Alpha
 }
--- tests/testthat/test-likelihoods.R
+++ tests/testthat/test-likelihoods.R
@@ -1,29 +1,114 @@
 test_that("Dirichlet alpha contruction routines work", {
   u <- create_universe(5, letters[1:5])
   N = RanCh::MMS_2019_counts[1, , ]
+
+  # Constant sum routine
   Alpha1 <- DirRC_constant_sum(5, 12.0, name_source = N)
   Alpha2 <- DirRC_constant_sum(5, 12.0, name_source = u)
-  # WJM: test constant shape routines here
   expect_identical(Alpha1, Alpha2)
+  expect_equal(Alpha1["abcde", "a"], 12/5)
+  expect_true(is.na(Alpha1["abcd", "e"]))
+
+  # Constant shape routine
+  Alpha1 <- DirRC_constant_shape(5, 1.0, name_source = N)
+  Alpha2 <- DirRC_constant_shape(5, 1.0, name_source = u)
+  expect_identical(Alpha1, Alpha2)
+  expect_equal(Alpha1["abcde", "a"], 1.0)
+  expect_true(is.na(Alpha1["abcd", "e"]))
 })
 
-test_that("Get correct value of max-min log marginal likelihood", {
+test_that("Simple P construction routines work", {
+  # P_uniform
+  P <- P_uniform(3)
+  expect_equal(P[3,1], 1/2)
+  expect_equal(P[7,1], 1/3)
+
+  # P_Luce
+  P_noname <- P_Luce(c(1,2,3))
+  P <- P_Luce(c(a=1, b=2, c=3))
+  expect_equal(P_noname, P, ignore_attr = TRUE)
+  expect_equal(P["abc", "a"], 1/6)
+
+  # P_logit
+  P_noname <- P_logit(c(-2,0,1))
+  P <- P_logit(c(a=-2, b=0, c=1))
+  expect_equal(P_noname, P, ignore_attr = TRUE)
+  expect_equal(P["abc", "a"], exp(-2)/(1+exp(-2)+exp(1)))
+
+  # P_frequencies
+  P <- P_frequencies(MMS_2019_counts[1,,])
+  expect_equal(P["abcde", "a"], 0.25)
+  expect_equal(P["abcde", "a"], P[31, 1])
+  expect_true(is.na(P["abcd", "e"]))
+})
+
+test_that("Values of marginal and maximum likelihoods are correct", {
   N <- RanCh::MMS_2019_counts[1, , ]
   P <- P_uniform(5)
+
+  # max-min log marginal likelihood
   max_min_ln_marl <- dmultinomRC(P, N, categorical=TRUE, log=TRUE)
   expect_equal(max_min_ln_marl, -1060.41961795424)
-})
 
-test_that("Get correct value of P log maximum likelihood",{
-  N <- RanCh::MMS_2019_counts[1, , ]
+  # P log maximum likelihod
   P <- P_frequencies(N)
   P_ln_maxl <- dmultinomRC(P, N, categorical=TRUE, log=TRUE)
   expect_equal(P_ln_maxl, -943.728101159082)
-})
 
-test_that("Get correct value of uniform P log marginal likelihood",{
-  N <- RanCh::MMS_2019_counts[1, , ]
+  # Uniform P log marginal likelihood
   Alpha <- DirRC_constant_shape(5, 1.0)
   uniform_P_ln_marl <- dDirMultinomRC(Alpha, N, categorical=TRUE, log=TRUE)
   expect_equal(uniform_P_ln_marl, -1017.67826542784)
 })
+
+test_that("{r,d}multinomRC work", {
+  v <- c(a=1, b=2, c=3)
+  n_objects <- length(v)
+  n_subsets <- 2^n_objects-1
+  n_trials <- 2
+  P <- P_Luce(v)
+  xy <- set_index(c(1, 2))      # Menu with x and y
+  xyz <- set_index(c(1, 2, 3))  # Menu with x, y, and z
+  N_total <- vector(mode="integer", length=n_subsets)
+  N_total[xy] <- 10    # Number of trials for menu with x and y
+  N_total[xyz] <- 12   # Number of trials for menu with x, y, and z
+  N <- rmultinomRC(n_trials, P, N_total)
+  expect_equal(N[xy, 1, 1] + N[xy, 2, 1], N_total[xy])
+  expect_equal(N[xyz, 1, 1] + N[xyz, 2, 1] + N[xyz, 3, 1], N_total[xyz])
+  lnf1 <- dmultinomRC(P, N[, , 1], categorical=TRUE, log=TRUE)
+  lnf2 <- dmultinomRC(P, N[, , 2], categorical=TRUE, log=TRUE)
+  expect_equal(dmultinomRC(P, N, categorical = TRUE, log=TRUE), lnf1 + lnf2)
+  expect_equal(dmultinomRC(P, N[, , 1], categorical=TRUE, log=FALSE), exp(lnf1))
+})
+
+test_that("{r,d}DirichletRC work", {
+  # Note that for constant shape of 1, P is uniform
+  Alpha <- DirRC_constant_shape(3, 1.0)  # 3 objects in universe
+  n_draws <- 5
+  P <- rDirichletRC(n_draws, Alpha)            # 5 draws
+  ln_L <- dDirichletRC(Alpha, P, log=TRUE)
+  expect_length(ln_L, n_draws)
+  expect_true(all(ln_L == ln_L[1])) # Relies on P uniform
+})
+
+test_that("{r,d}DirMultinomRC work", {
+  v <- c(a=1, b=2, c=3)
+  n_objects <- length(v)
+  n_subsets <- 2^n_objects-1
+  n_trials <- 2
+  Alpha <- DirRC_constant_shape(n_objects, 1.0)
+  xy <- set_index(c(1, 2))      # Menu with x and y
+  xyz <- set_index(c(1, 2, 3))  # Menu with x, y, and z
+  N_total <- vector(mode="integer", length=n_subsets)
+  N_total[xy] <- 10    # Number of trials for menu with x and y
+  N_total[xyz] <- 12   # Number of trials for menu with x, y, and z
+  N <- rDirMultinomRC(n_trials, Alpha, N_total)
+  expect_equal(N[xy, 1, 1] + N[xy, 2, 1], N_total[xy])
+  expect_equal(N[xyz, 1, 1] + N[xyz, 2, 1] + N[xyz, 3, 1], N_total[xyz])
+  lnf1 <- dDirMultinomRC(Alpha, N[, , 1], categorical=TRUE, log=TRUE)
+  lnf2 <- dDirMultinomRC(Alpha, N[, , 2], categorical=TRUE, log=TRUE)
+  expect_equal(dDirMultinomRC(Alpha, N, categorical = TRUE, log=TRUE), lnf1 + lnf2)
+  expect_equal(dDirMultinomRC(Alpha, N[, , 1], categorical=TRUE, log=FALSE), exp(lnf1))
+})
+
+
